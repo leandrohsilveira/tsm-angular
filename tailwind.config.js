c40d523fd8152f84a640f3dc1922c4e5e5ecf29b
Leandro Hinckel Silveira <leandro.hinckel@ilia.digital>
2025-02-07 14:42:42 -0300

    format code

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import aspectRatio from "@tailwindcss/aspect-ratio"
+import containerQueries from "@tailwindcss/container-queries"
+import typography from "@tailwindcss/typography"
+import daisyui from "daisyui"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: [typography, containerQueries, aspectRatio, daisyui],
+  daisyui: {}
+}
